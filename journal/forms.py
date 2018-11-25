24851c68af645f8ccac2b0327c4d27d02c7c5e06
Hayley <hayleyma16@gmail.com>
2018-11-25 14:53:52 -0600

    Entry form formatting

--- journal/forms.py
+++ journal/forms.py
@@ -3,4 +3,5 @@ from django import forms
 #Entry Form is a basic form object with a CharField
 #used for writing a journal entry
 class EntryForm(forms.Form):
-    entry_form = forms.CharField(label="How ya doin'?")
+    entry_form = forms.CharField(widget=forms.Textarea)
+    #comment = forms.CharField(widget=forms.Textarea)
