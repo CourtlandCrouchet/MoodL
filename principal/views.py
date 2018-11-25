2e8eaf703cfa7aab88585593490fbf283c0093ff
Unknown <kemingzhang110@gmail.com>
2018-11-25 17:10:13 -0600

    delete useless code in views.py

--- principal/views.py
+++ principal/views.py
@@ -31,25 +31,12 @@ def login(request):
         return render_to_response('main/main_base.html', dictionary)
 
 
-#def signup(request):
- #   if request.method == 'POST':
-  #      username = request.POST.get('name')
-   #     pass_1 = request.POST.get('password1')
-    #    pass_2 = request.POST.get('password2')
-     #      user = User.objects.create_user(username=username, password=pass_1)
-      #      dictionary = dict(request=request)
-       #     dictionary.update(csrf(request))
-        #    return render_to_response('main/main_base.html', dictionary)
-        #else:
-         #   msg_to_html = custom_message('Invalid Credentials', TagType.danger)
-          #  dictionary = dict(request=request, messages=msg_to_html)
-           # dictionary.update(csrf(request))
-        #return render_to_response('account/signup.html', dictionary)
-
 class signup(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('get_entry')
     template_name = 'signup.html'
+
+
 
 # this def is if you want to change the user's password
 def update_pwd(username, pwd):
