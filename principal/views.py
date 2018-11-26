1fd64985bd8c95792d5bcffaa154079e390634d0
Unknown <CourtlandCthulhu@gmail.com>
2018-11-26 12:47:59 -0600

    Fixed

--- principal/views.py
+++ principal/views.py
@@ -43,11 +43,16 @@ def login(request):
 #reverse_lazy because that for all generic class-based views the urls are
 #not loaded when the file is imported, so we use the lazy form of revers
 #rather than reverse.
-class signup(generic.CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy('get_entry')
-    template_name = 'signup.html'
 
+#class signup(generic.CreateView):
+    #form_class = UserCreationForm
+    #success_url = reverse_lazy('new_entry')
+    #template_name = 'signup.html'
+def signup(request):
+    if request.method == 'GET':
+        return render(request, 'signup.html')
+    else:
+        return HttpResponseRedirect('/journal/new_entry')
 #this links the user to the about page.
 def about(request):
         return render(request, 'help.html')
